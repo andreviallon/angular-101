50ff55490a1eb26a7ff56d4652988b3ff18ed2b1
andreviallon <viallon.andre@gmail.com>
2018-07-03 13:48:51 +0200

    Pipe operators
    pipe operators used to manupulate the outputed text

--- src/app/course/course.component.ts
+++ src/app/course/course.component.ts
@@ -7,6 +7,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CourseComponent implements OnInit {
 
+  course = {
+    title: 'The Complete Angular Course',
+    rating: 4.9745,
+    students: 30123,
+    price: 190.25,
+    releaseDate: new Date(2016, 3, 1)
+  };
+
   constructor() { }
 
   ngOnInit() {
