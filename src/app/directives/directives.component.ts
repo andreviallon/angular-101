e457f0e3c9bfab03d73965a51c28be3ae190a8b5
andreviallon <viallon.andre@gmail.com>
2018-07-05 15:32:42 +0200

    *ngFor & change detection

--- src/app/directives/directives.component.ts
+++ src/app/directives/directives.component.ts
@@ -7,12 +7,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DirectivesComponent implements OnInit {
   courses = [
-    { id: 1, name: 'couse 1 ' },
-    { id: 2, name: 'couse 2 ' },
-    { id: 3, name: 'couse 3 ' }
+    { id: 1, name: 'course 1 ' },
+    { id: 2, name: 'course 2 ' },
+    { id: 3, name: 'course 3 ' }
   ];
 
   viewMode = 'map';
+
+  onAdd() {
+    this.courses.push({ id: 4, name: 'course 4' });
+  }
+
+  onRemove(course) {
+    const index = this.courses.indexOf(course);
+    this.courses.splice(index, 1);
+  }
 
   constructor() { }
 
