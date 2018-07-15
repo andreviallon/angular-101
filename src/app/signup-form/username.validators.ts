5848618bbb46b635f7e1b6d0a1176be3e944f43a
andreviallon <viallon.andre@gmail.com>
2018-07-15 21:28:13 +0200

    Asynchronous Validators

--- src/app/signup-form/username.validators.ts
+++ src/app/signup-form/username.validators.ts
@@ -1,4 +1,5 @@
 import { AbstractControl, ValidationErrors } from '@angular/forms';
+import { resolve } from 'url';
 
 export class UsernameValidators {
   static cannotContainSpace(control: AbstractControl): ValidationErrors | null {
@@ -6,5 +7,16 @@ export class UsernameValidators {
       return { cannotContainSpace: true };
     }
     return null;
+  }
+
+  static shouldBeUnique(control: AbstractControl): Promise<ValidationErrors | null> {
+    // tslint:disable-next-line:no-shadowed-variable
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        if (control.value === 'andre') {
+          resolve({ shouldBeUnique: true });
+        } else { resolve(null); }
+      }, 2000);
+    });
   }
 }
