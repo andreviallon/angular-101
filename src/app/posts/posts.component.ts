e12e2e47dd144b3c3f1b314894fca0430146aa51
andreviallon <viallon.andre@gmail.com>
2018-07-18 16:45:05 +0200

    Use map operator in CRUD

--- src/app/posts/posts.component.ts
+++ src/app/posts/posts.component.ts
@@ -21,10 +21,9 @@ export class PostsComponent implements OnInit {
 
     this.service.create(post)
       .subscribe(
-        response => {
-          post['id'] = response.json().id;
+        newPost => {
+          post['id'] = newPost.id;
           this.posts.splice(0, 0, post);
-          console.log(response.json());
         },
         (error: AppError) => {
           if (error instanceof BadInput) {
@@ -38,15 +37,15 @@ export class PostsComponent implements OnInit {
   updatePost(post) {
     this.service.update(post)
       .subscribe(
-        response => {
-          console.log(response.json());
+        updatedPost => {
+          console.log(updatedPost);
         });
   }
 
   deletePost(post) {
     this.service.delete(333)
       .subscribe(
-        response => {
+        () => {
           const index = this.posts.indexOf(post);
           this.posts.splice(index, 1);
         },
@@ -61,10 +60,7 @@ export class PostsComponent implements OnInit {
 
   ngOnInit() {
     this.service.getAll()
-      .subscribe(
-        response => {
-          this.posts = response.json();
-        });
+      .subscribe(posts => this.posts = posts);
   }
 
 }
